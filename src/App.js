663127ebb33f60439e3af1559e47e018640b429b
Wenxiao <wenxiao1019@gmail.com>
2019-02-27 00:00:33 -0800

    Remove unneed logic in App

--- src/App.js
+++ src/App.js
@@ -4,18 +4,6 @@ import Header from './components/test/Header';
 import MouseTracker from './components/test/MouseTracker';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { x: 0, y: 0 };
-  }
-
-  handleMouseMove = (event) => {
-    this.setState({
-      x: event.clientX,
-      y: event.clientY
-    });
-  }
-
   render() {
     return (
       <div>
