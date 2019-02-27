a81ea9fa0a38a7ce606aea0cf83426556dd50479
Wenxiao <wenxiao1019@gmail.com>
2019-02-26 23:59:30 -0800

    Add MouseTracker

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
-import Header from './components/Header';
+import Header from './components/test/Header';
+import MouseTracker from './components/test/MouseTracker';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { x: 0, y: 0 };
+  }
+
+  handleMouseMove = (event) => {
+    this.setState({
+      x: event.clientX,
+      y: event.clientY
+    });
+  }
+
   render() {
     return (
-      <Header />
+      <div>
+        <Header />
+        <MouseTracker />
+      </div>
     );
   }
 }
---
+++ src/components/test/MouseTracker.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class MouseTracker extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = { x: 0, y: 0 };
+  }
+
+  handleMouseMove = (event) => {
+    this.setState({
+      x: event.clientX,
+      y: event.clientY
+    });
+  }
+
+  render() {
+    return (
+      <div onMouseMove={this.handleMouseMove}>
+        <h1>Move the mouse around!</h1>
+        <p>The current mouse position is ({this.state.x}, {this.state.y})</p>
+      </div>
+    );
+  }
+}
+
+export default MouseTracker;
